1499ea714ac48179db62c7519a7ac60701b6b8a2
hyenee99 <twoddal2@naver.com>
2024-10-04 11:22:19 +0900

    로그인 api 구현

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,5 +1,8 @@
 const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken'); //jwt 모듈
+const dotenv = require('dotenv');
+dotenv.config();
 
 const join = (req, res) => {
   const { email, password } = req.body;
@@ -17,4 +20,47 @@ const join = (req, res) => {
     })
 }
 
-module.exports = join;
+const login = (req, res) => {
+  const { email, password } = req.body;
+  let sql = `SELECT * FROM users WHERE email =? `;
+  conn.query(sql, email,
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
+
+      const loginUser = results[0];
+      if (loginUser && loginUser.password == password) {
+        // 토큰 발행 
+        const token = jwt.sign({
+          email: loginUser.email
+        }, process.env.PRIVATE_KEY, {
+          expiresIn: '5m',
+          issuer: "hyewon"
+        });
+
+        // 토큰 쿠키에 담기
+        res.cookie("token", token, {
+          httpOnly: true
+        });
+        console.log(token);
+
+        return res.status(StatusCodes.OK).json(results);
+      }
+      else {
+        return res.status(StatusCodes.UNAUTHORIZED).end();
+      }
+      
+    })
+};
+
+const passwordResetRequest = (req, res) => {
+
+};
+
+const passwordReset = (req, res) => {
+
+};
+
+module.exports = { join, login, passwordResetRequest, passwordReset };
