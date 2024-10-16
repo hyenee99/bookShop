29791fbb98699616a9ca4567ae8f67a45d7d3a33
hyenee99 <twoddal2@naver.com>
2024-10-16 16:42:14 +0900

    주문하기 api 구현

--- mariadb.js
+++ mariadb.js
@@ -2,13 +2,14 @@
 const mysql = require('mysql2');
 
 // Create the connection to database
-const connection = mysql.createConnection({
+
+const conn = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'root',
   database: 'Bookshop',
-  port: '3307' ,// mariadb 포트 번호 3307로 변경했음
+  port: '3307',// mariadb 포트 번호 3307로 변경했음
   dateStrings: true
 });
 
-module.exports=connection
+module.exports = conn;
