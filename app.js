009e2d3e9ddcc03ccb682fcf66ce62bec2f6d1a3
hyenee99 <twoddal2@naver.com>
2024-11-16 00:00:51 +0900

    CORS 설정

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const dotenv = require('dotenv');
 dotenv.config();
+
+// CORS 설정
+const corsOptions = {
+  origin: 'http://localhost:3000',  // 리액트 앱의 주소
+  credentials: true             // 쿠키 사용 시 필요
+};
+
+// CORS 미들웨어 적용
+app.use(cors(corsOptions));
 
 app.listen(process.env.PORT);
 
